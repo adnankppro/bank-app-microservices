df570d77502b8cc017cddb48d3932c1e497c6892
Adnan <adnankppro@gmail.com>
2026-07-08 11:17:27 +0530

    Initial commit

---
+++ user-profile/src/main/java/com/adnan/bankapp/user_profile/UserProfileApplication.java
@@ -0,0 +1,13 @@
+package com.adnan.bankapp.user_profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileApplication.class, args);
+	}
+
+}
